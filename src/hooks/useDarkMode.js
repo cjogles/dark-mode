19be9284df14d40cb25ebea626b18bd2a8615ba4
cjogles <jackson.ogles28@gmail.com>
2019-10-30 18:30:19 -0600

    finished MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,12 +1,8 @@
 import { useLocalStorage } from './useLocalStorage';
-import { useEffect } from 'react';
 
-export function useDarkMode(key, initialValue) {
+export const useDarkMode = (key, initialValue) => {
     const [someValue, setSomeValue] = useLocalStorage(key, initialValue)
-    useEffect(() => {
-        //check to see if the value from `useLocalStorage` is true or false
-        //If it's true, add the class `dark-mode` to the `body` element
-        //If it's false, remove the class from the `body` element.
-        
-      });
+    const body = document.querySelector('body');
+        someValue ? body.classList.add('dark-mode'): body.classList.remove("dark-mode");
+      return [someValue, setSomeValue];
 }
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function useLocalStorage (key, initialValue) {
+export const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
@@ -11,3 +11,4 @@ export function useLocalStorage (key, initialValue) {
     }
     return [storedValue, setValue];
 }
+
